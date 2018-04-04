c8ce833869f78114012a844c5ca7097b4cd9b480
Wei Wei <weiwei@microsoft.com>
2018-04-04 14:04:41 +0800

    [Storage] Support Immunablity Policy

---
+++ src/ResourceManager/Storage/Commands.Management.Storage/Blob/EnableAzureStorageContainerLease.cs
@@ -0,0 +1,62 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Management.Storage.Models;
+using Microsoft.Azure.Management.Storage;
+using Microsoft.Azure.Management.Storage.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.Commands.Management.Storage
+{
+    [Cmdlet(VerbsLifecycle.Enable, StorageContainerLeaseNounStr)]
+    public class EnableStorageContainerLeaseCommand : StorageBlobBaseCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Resource Group Name.")]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Storage Account Name.")]
+        [Alias(AccountNameAlias)]
+        [ValidateNotNullOrEmpty]
+        public string StorageAccountName { get; set; }
+
+        [Alias("N", "Container")]
+        [Parameter(Position = 0, HelpMessage = "Container Name",
+            ValueFromPipeline = true,
+             ValueFromPipelineByPropertyName = true)]
+        public string Name { get; set; }      
+
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+            
+
+            this.StorageClient.BlobContainers.Lease(
+                        this.ResourceGroupName,
+                        this.StorageAccountName,
+                        this.Name);
+        }
+    }
+}
